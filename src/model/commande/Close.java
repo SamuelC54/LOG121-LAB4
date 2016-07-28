9cdb2edfa6db5c894895264a730f85678008f931
Polocapt <torresbriseboisphilippe@gmail.com>
2016-07-27 23:21:47 -0400

    remaining javadoc

--- src/model/commande/Close.java
+++ src/model/commande/Close.java
@@ -1,3 +1,23 @@
+/******************************************************
+Cours:   LOG121
+Session: E2016
+Groupe:  01
+Projet: Laboratoire #4
+Étudiant(e)s: 
+              Philippe Torres-Brisebois
+              Laurent Theroux-Bombardier
+              Samuel Croteau
+              Nelson Chao
+Professeur : Francis Cardinal
+Nom du fichier: Close.java
+Date créé: 2016-07-27
+Date dern. modif. 2016-07-27
+*******************************************************
+Historique des modifications
+*******************************************************
+2016-07-27 Version initiale
+*******************************************************/
+
 package model.commande;
 
 import java.awt.event.WindowEvent;
@@ -7,10 +27,18 @@ import javax.swing.JFrame;
 public class Close implements Commande {
 	private JFrame frame;
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param frame the frame to close
+	 */
 	public Close(JFrame frame){
 		this.frame = frame;
 	}
 
+	/**
+	 * Close the current frame
+	 */
 	public void execute() {
 		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	}
